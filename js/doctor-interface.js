82481fdc388cc6964971929b62d1935f1b5d14cf
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-06 11:46:41 -0800

    attempt to make Doctor and AllDoctors objects so that each new doctor may be pushed to an AllDoctors array.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,21 +1,29 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
+var AllDoctors = require('./../js/doctor.js').allModule;
 
-var displayDoctors = function(img, firstName, lastName, title, specialty) {
-  var image = ('<img src=' + img + '>');
-  $('#doctor-ul').append("<li class='collection-item'>" + image + " " + firstName + " " + lastName + ", " + title + " " + specialty);
+var newDoctor = function(img, firstName, lastName, title, specialty, rating) {
+  var doctor = Doctor.new(img, firstName, lastName, title, specialty, rating);
+  AllDoctors.doctors.push(doctor);
 };
 
 $(document).ready(function(){
-
-  $('#find-doctor').click(function(){
-    $('#doctor-ul').empty();
-    var currentDoctorObject = new Doctor();
-    var medicalIssue = $('#illness-input').val();
-    currentDoctorObject.getDoctor(medicalIssue, displayDoctors);
-    $('.collection-header').show();
-    medicalIssue = $('#illness-input').val("");
-  });
-
+  var doctors = Doctor.getDoctor('toothache', newDoctor);
+  console.log(doctors);
 });
-
-// exports.getDoctors("toothache");
+// var displayDoctors = function(img, firstName, lastName, title, specialty, rating) {
+//   var image = ('<img src=' + img + '>');
+//   $('#doctor-ul').append("<li class='collection-item'>" + image + " " + firstName + " " + lastName + ", " + title + " " + specialty);
+// };
+//
+// $(document).ready(function(){
+//
+//   $('#find-doctor').click(function(){
+//     $('#doctor-ul').empty();
+//     var currentDoctorObject = new Doctor();
+//     var medicalIssue = $('#illness-input').val();
+//     currentDoctorObject.getDoctor(medicalIssue, displayDoctors);
+//     $('.collection-header').show();
+//     medicalIssue = $('#illness-input').val("");
+//   });
+//
+// });
--- js/doctor.js
+++ js/doctor.js
@@ -1,16 +1,39 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor (){
+function Doctor (img, firstName, lastName, title, specialty, rating) {
+  this.img = img;
+  this.firstName = firstName;
+  this.lastName = lastName;
+  this.title = title;
+  this.specialty = specialty;
+  this.rating = rating;
 }
 
-Doctor.prototype.getDoctor = function(medicalIssue, displayFunction) {
+function AllDoctors () {
+  this.doctors = [];
+}
+
+Doctor.prototype.getDoctor = function(medicalIssue, newDoctor) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
        for(i =0; i <= 19; i ++){
-         displayFunction(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
+         newDoctor(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
        }
       }).fail(function(error){
         console.log(error.message);
       });
   };
 
+// function Doctor (){
+// }
+//
+// Doctor.prototype.getDoctor = function(medicalIssue, displayFunction) {
+//     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
+//        for(i =0; i <= 19; i ++){
+//          displayFunction(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
+//        }
+//       }).fail(function(error){
+//         console.log(error.message);
+//       });
+//   };
+exports.allModule = AllDoctors;
 exports.doctorModule = Doctor;
