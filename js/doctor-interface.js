ea7032f2149f17b260f30d10c5df3be89ac1286e
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-06 13:43:13 -0800

    return code to previous functional version.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,27 +1,19 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
-var AllDoctors = require('./../js/doctor.js').allModule;
+
+var displayDoctors = function(img, firstName, lastName, title, specialty, rating) {
+  var image = ('<img src=' + img + '>');
+  $('#doctor-ul').append("<li class='collection-item'>" + image + " " + firstName + " " + lastName + ", " + title + " " + specialty);
+};
 
 $(document).ready(function(){
-  var doctor = new Doctor();
-  var allDoctors = new AllDoctors();
-  doctor.getDoctor('toothache', doctor, allDoctors);
-  console.log(doctor);
-  console.log(allDoctors);
+
+  $('#find-doctor').click(function(){
+    $('#doctor-ul').empty();
+    var currentDoctorObject = new Doctor();
+    var medicalIssue = $('#illness-input').val();
+    currentDoctorObject.getDoctor(medicalIssue, displayDoctors);
+    $('.collection-header').show();
+    medicalIssue = $('#illness-input').val("");
+  });
+
 });
-// var displayDoctors = function(img, firstName, lastName, title, specialty, rating) {
-//   var image = ('<img src=' + img + '>');
-//   $('#doctor-ul').append("<li class='collection-item'>" + image + " " + firstName + " " + lastName + ", " + title + " " + specialty);
-// };
-//
-// $(document).ready(function(){
-//
-//   $('#find-doctor').click(function(){
-//     $('#doctor-ul').empty();
-//     var currentDoctorObject = new Doctor();
-//     var medicalIssue = $('#illness-input').val();
-//     currentDoctorObject.getDoctor(medicalIssue, displayDoctors);
-//     $('.collection-header').show();
-//     medicalIssue = $('#illness-input').val("");
-//   });
-//
-// });
--- js/doctor.js
+++ js/doctor.js
@@ -1,53 +1,16 @@
 var apiKey = require('./../.env').apiKey;
 
-function Doctor () {
-  this.img = "";
-  this.firstName = "";
-  this.lastName = "";
-  this.title = "";
-  this.specialty = "";
-  this.rating = "";
+function Doctor (){
 }
 
-function AllDoctors () {
-  this.doctors = [];
-}
-
-Doctor.prototype.newDoctor = function(doctor, img, firstName, lastName, title, specialty, rating) {
-  doctor.img = img;
-  doctor.firstName = firstName;
-  doctor.lastName = lastName;
-  doctor.title = title;
-  doctor.specialty = specialty;
-  doctor.rating = rating;
-};
-
-Doctor.prototype.getDoctor = function(medicalIssue, doctor, allDoctors) {
+Doctor.prototype.getDoctor = function(medicalIssue, displayFunction) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
        for(i =0; i <= 19; i ++){
-         var newDoctor =doctor.newDoctor(doctor, result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
-         allDoctors.doctors.push(newDoctor);
-
-
-
-        //  newDoctor(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
+         displayFunction(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
        }
       }).fail(function(error){
         console.log(error.message);
       });
   };
 
-// function Doctor (){
-// }
-//
-// Doctor.prototype.getDoctor = function(medicalIssue, displayFunction) {
-//     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
-//        for(i =0; i <= 19; i ++){
-//          displayFunction(result.data[i].profile.image_url, result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title, result.data[i].specialties[0].name);
-//        }
-//       }).fail(function(error){
-//         console.log(error.message);
-//       });
-//   };
-exports.allModule = AllDoctors;
 exports.doctorModule = Doctor;
