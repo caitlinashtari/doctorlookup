e57e7bd2e66565ff5c28b3aad8a6fbc94829651d
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-06 09:29:48 -0800

    refactor js files to remove jquery from backend and instead use callback function to retrieve api response.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,11 +1,14 @@
 var Doctor = require('./../js/doctor.js').doctorModule;
 
+var displayDoctors = function(firstName, lastName, title) {
+  $('#doctor-ul').append("<li class='collection-item'>" + firstName + " " + lastName + ", " + title);
+}
 
 $(document).ready(function(){
   var currentDoctorObject = new Doctor();
   $('#find-doctor').click(function(){
     var medicalIssue = $('#illness-input').val();
-    currentDoctorObject.getDoctor(medicalIssue);
+    currentDoctorObject.getDoctor(medicalIssue, displayDoctors);
     $('.collection-header').show();
   });
 
--- js/doctor.js
+++ js/doctor.js
@@ -3,10 +3,10 @@ var apiKey = require('./../.env').apiKey;
 function Doctor (){
 }
 
-Doctor.prototype.getDoctor = function(medicalIssue) {
+Doctor.prototype.getDoctor = function(medicalIssue, displayFunction) {
     $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey).then(function(result) {
        for(i =0; i <= 19; i ++){
-        $('#doctor-ul').append("<li class='collection-item'>" + result.data[i].profile.first_name + " " + result.data[i].profile.last_name + ", " + result.data[i].profile.title + "</li>");
+         displayFunction(result.data[i].profile.first_name, result.data[i].profile.last_name, result.data[i].profile.title);
        }
       }).fail(function(error){
         console.log(error.message);
