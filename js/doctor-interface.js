41a709a794e102c07d488bc3a34a3bcb6ef0fa71
Caitlin Ashtari <caitlinashtari@gmail.com>
2017-01-06 08:36:04 -0800

    add input field to index.html, separate frontend js from backend js, and click functionality to display doctors in console.

--- js/doctor-interface.js
+++ js/doctor-interface.js
@@ -1,14 +1,13 @@
-var apiKey = require('./../.env').apiKey;
+var Doctor = require('./../js/doctor.js').doctorModule;
 
-exports.getDoctors = function(medicalIssue) {
-  $.get('https://api.betterdoctor.com/2016-03-01/doctors?query=' + medicalIssue + '&location=45.5231%2C-122.6765%2C%205&user_location=45.5231%2C-122.6765&skip=0&limit=20&user_key=' + apiKey)
-   .then(function(result) {
-     for(i =0; i <= 19; i ++){
-      console.log(result.data[i].profile.first_name);
-     }
-    }).fail(function(error){
-      console.log(error.message);
-    });
-};
 
-exports.getDoctors("toothache");
+$(document).ready(function(){
+  var currentDoctorObject = new Doctor();
+  $('#find-doctor').click(function(){
+    var medicalIssue = $('#illness-input').val();
+    currentDoctorObject.getDoctor(medicalIssue);
+  });
+
+});
+
+// exports.getDoctors("toothache");
